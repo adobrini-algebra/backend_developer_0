f96bc6c102614778583c7c863b6435944da08e38
Aleksandar Dobrinic <aleksandar.dobrinic@predavaci.algebra.hr>
2024-10-28 20:01:52 +0100

    dokerizajija projekta

--- napredni_php/config/database.php
+++ napredni_php/config/database.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'dbname' => 'videoteka',
+    'host' => 'database',
+    'dbname' => 'videoteka-old',
     'user' => 'algebra',
     'password' => 'algebra',
     'charset' => 'utf8mb4',
+    'port' => 3306,
     'options' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
